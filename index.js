6b35cd5c7a8db675c2b08a0b03505a5d417497af
seekwhencer <seekwhencer>
2019-02-14 16:12:12 +0100

    pihole

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const
-    DnsMasq = require('./lib/dnsmasq'),
     Api = require('./lib/api');
 
 require('./lib/globals');
 
-global.DNSMASQ = new DnsMasq();
+if (global.ENV === 'pihole') {
+    const Pihole = require('./lib/pihole');
+    global.ENGINE = new Pihole();
+} else {
+    const DnsMasq = require('./lib/dnsmasq');
+    global.ENGINE = new DnsMasq();
+}
+
+//global.DNSMASQ = new DnsMasq();
 global.API = new Api();
